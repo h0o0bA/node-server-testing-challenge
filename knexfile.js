edfa9e1cbc8e58854c8c7d26583bb5c148703e22
Ihab Ali <ihab@live.com>
2019-08-29 02:42:26 -0500

    added testing db configuration to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -20,5 +20,24 @@ module.exports = {
         conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       }
     }
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
+    }
   }
 };
