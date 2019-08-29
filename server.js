521f931c96b44f6a6518781f2b9b3ef705eda278
Ihab Ali <ihab@live.com>
2019-08-29 04:40:10 -0500

    added post and delete /hobbits endpoints

--- server.js
+++ server.js
@@ -20,7 +20,40 @@ server.get("/hobbits", (req, res) => {
       res.status(200).json(hobbits);
     })
     .catch(error => {
-      res.status(500).json(error);
+      res
+        .status(500)
+        .json({ message: "There was an error getting the hobbits." });
+    });
+});
+
+server.post("/hobbits", (req, res) => {
+  const hobbit = req.body;
+  HobbitsDb.add(hobbit)
+    .then(hobbit => {
+      res.status(201).json(hobbit);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "There was an error adding the hobbit." });
+    });
+});
+
+server.delete("/hobbits/:id", (req, res) => {
+  const { id } = req.params;
+
+  HobbitsDb.remove(id)
+    .then(hobbit => {
+      if (hobbit) {
+        res.status(200).json(hobbit);
+      } else {
+        res.status(404).json({
+          message: "The hobbit with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "The hobbit could not be removed." });
     });
 });
 
