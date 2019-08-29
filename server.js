ab786aecb4dbd06f97a1168c01d60664445fa6ae
Ihab Ali <ihab@live.com>
2019-08-29 03:52:58 -0500

    added get /hobbits endpoint by using getAll() access method

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const cors = require("cors");
 
 const server = express();
 
+const HobbitsDb = require("./hobbits/hobbitsModel.js");
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 server.get("/", (req, res) => {
   res.send("<h2>Node Server Testing Challenge!</h2>");
+});
+
+server.get("/hobbits", (req, res) => {
+  HobbitsDb.getAll()
+    .then(hobbits => {
+      res.status(200).json(hobbits);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 module.exports = server;
